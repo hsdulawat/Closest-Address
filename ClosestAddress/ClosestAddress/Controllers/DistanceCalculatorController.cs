9bb240d661ad7e38595449693b704bb36110571f
Himmat Singh Dulawat <himmat20987@gmail.com>
2019-11-24 16:50:04 +0530

    Optimize code

--- ClosestAddress/ClosestAddress/Controllers/DistanceCalculatorController.cs
+++ ClosestAddress/ClosestAddress/Controllers/DistanceCalculatorController.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Net;
 using System.Web.Http;
 using System.Xml.Linq;
-using System.Linq;
 namespace ClosestAddress.Controllers
 {
     public class DistanceCalculatorController : ApiController
@@ -50,35 +49,6 @@ namespace ClosestAddress.Controllers
             }
             return distanceInKM;
         }
-        [HttpGet]
-        public List<Address> Get(string originAddress)
-        {
-            List<Address> addresses = new List<Address>();
-            var addressList = GetAllAddresses();
-            foreach (var destinationAddress in addressList)
-            {
-                if (!string.IsNullOrEmpty(destinationAddress))
-                {
-                    string km = GetLocation(originAddress, destinationAddress);
-                    if (string.IsNullOrEmpty(km))
-                    {
-                        var list = km.Split(' ');
-                        if (list.Length > 0 && !string.IsNullOrEmpty(list[0]))
-                        {
-                            var item = new Address();
-                            item.KM = list[0];
-                            item.Name = destinationAddress;
-                            addresses.Add(item);
-                        }
-                    }
-                }
-            }
-            if (addresses.Count > 0)
-            {
-                addresses = addresses.OrderBy(x => x.KM).Take(5).ToList();
-            }
-            return addresses;
-        }
         //[HttpGet]
         //public List<Address> Get(string originAddress)
         //{
@@ -86,11 +56,40 @@ namespace ClosestAddress.Controllers
         //    var addressList = GetAllAddresses();
         //    foreach (var destinationAddress in addressList)
         //    {
-        //        var item = new Address();
-        //        item.Name = destinationAddress;
-        //        addresses.Add(item);
+        //        if (!string.IsNullOrEmpty(destinationAddress))
+        //        {
+        //            string km = GetLocation(originAddress, destinationAddress);
+        //            if (string.IsNullOrEmpty(km))
+        //            {
+        //                var list = km.Split(' ');
+        //                if (list.Length > 0 && !string.IsNullOrEmpty(list[0]))
+        //                {
+        //                    var item = new Address();
+        //                    item.KM = list[0];
+        //                    item.Name = destinationAddress;
+        //                    addresses.Add(item);
+        //                }
+        //            }
+        //        }
+        //    }
+        //    if (addresses.Count > 0)
+        //    {
+        //        addresses = addresses.OrderBy(x => x.KM).Take(5).ToList();
         //    }
         //    return addresses;
         //}
+        [HttpGet]
+        public List<Address> Get(string originAddress)
+        {
+            List<Address> addresses = new List<Address>();
+            var addressList = GetAllAddresses();
+            foreach (var destinationAddress in addressList)
+            {
+                var item = new Address();
+                item.Name = destinationAddress;
+                addresses.Add(item);
+            }
+            return addresses;
+        }
     }
 }
